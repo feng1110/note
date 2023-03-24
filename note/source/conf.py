2b4b7577ecb80629c005bcc9430f6862022d821c
feng1110 <128796068+feng1110@users.noreply.github.com>
2023-03-24 19:47:13 +0800

    Add files via upload

--- note/source/conf.py
+++ note/source/conf.py
@@ -5,6 +5,24 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# At top on conf.py (with other import statements)
+import recommonmark
+from recommonmark.transform import AutoStructify
+
+# At the bottom of conf.py
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+            'url_resolver': lambda url: github_doc_root + url,
+            'auto_toc_tree_section': 'Contents',
+            }, True)
+    app.add_transform(AutoStructify)
+
+from recommonmark.parser import CommonMarkParser
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+source_suffix = ['.rst', '.md']
+
 
 project = 'notes'
 copyright = '2023, wwb'
@@ -26,3 +44,4 @@ language = 'zh_CN'
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
